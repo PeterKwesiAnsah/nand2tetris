15a8b360e18796b73dceb02efd9b5ba523f6a42b
peterkwesiansah <ansahpeter123@yahoo.com>
2025-01-01 17:47:37 +0000

    feat: parser module

---
+++ project6/parser.c
@@ -0,0 +1,111 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/_types/_null.h>
+
+#include "parser.h"
+#include "tokenizer.h"
+
+#define CHAR_START_AINS '@'
+#define CHAR_START_LABEL '('
+#define DEST_OPERATOR '='
+#define JUMP_OPERATOR ';'
+#define ALLOC_FACTOR 2
+
+
+size_t resize(struct Tokens *tokenXS){
+    size_t newCap;
+    if(tokenXS->capacity==0){
+        tokenXS->darray=NULL;
+        newCap=256;
+    }else{
+      newCap=tokenXS->capacity * 2;
+    }
+    tokenXS->darray=(struct token **)realloc(tokenXS->darray, newCap * sizeof(struct token *));
+    return newCap;
+};
+void append(struct Tokens *tokenXS, struct token *p){
+    if(tokenXS->count>=tokenXS->capacity){
+       size_t newSize= resize(tokenXS);
+        ASSERT_MALLOC(tokenXS->darray, "failed to allocate memory\n")
+        tokenXS->capacity=newSize;
+    }
+    tokenXS->darray[tokenXS->count++]=p;
+};
+
+
+int main(void){
+   struct ins *cur=tokenizer();
+    //initizializing tokens dynamic array
+   struct Tokens ins = {0, 0,NULL};
+
+    while(cur){
+        int startOfIns=cur->instruction[0];
+        if(startOfIns==CHAR_START_AINS){
+            //A instruction -> @Value
+            struct token *AIns=(struct token*)malloc(sizeof(struct token));
+            ASSERT_MALLOC(AIns, "failed to allocate memory\n")
+            char *curInstructionAddress=cur->instruction;
+            AIns->tokenKind=A;
+            //TODO: free memory occupied by link node??
+            AIns->tokenType.address=(char *)malloc(sizeof(*cur->instruction)*cur->n);
+            ASSERT_MALLOC(AIns->tokenType.address, "failed to allocate memory\n")
+            memcpy(AIns->tokenType.address, ++curInstructionAddress,cur->n);
+            append(&ins, AIns);
+            //add to map
+        }else if (startOfIns== CHAR_START_LABEL) {
+            //add to map
+        }else {
+            //C instruction -> dest=comp;jump
+            struct token *CIns=(struct token*)malloc(sizeof(struct token));
+            ASSERT_MALLOC(CIns, "failed to allocate memory\n")
+            CIns->tokenKind=C;
+            CIns->tokenType.c=(struct CIns*)calloc(1,sizeof(struct CIns));
+            ASSERT_MALLOC(CIns->tokenType.c, "failed to allocate memory\n");
+
+            char *insP=cur->instruction;
+            char curChar=*insP;
+
+            char *destOperatorP=NULL;
+            char *jumpOperatorP=NULL;
+            char *nullCharP=cur->instruction+cur->n;
+
+            while(curChar){
+                if(curChar==DEST_OPERATOR){
+                destOperatorP=insP;
+                }else if (curChar==JUMP_OPERATOR) {
+                    jumpOperatorP=insP;
+                }
+                insP++;
+                curChar=*insP;
+            }
+
+            if(destOperatorP){
+                //handing dest
+                //TODO: we can use parameteriezed macro
+                char *dest=CIns->tokenType.c->dest;
+                char *src=cur->instruction;
+                memcpy(dest, src, destOperatorP-src);
+                //handing comp
+                {
+                    //TODO: we can use parameteriezed macro
+                    char *dest=CIns->tokenType.c->comp;
+                    char *src=++destOperatorP;
+                    memcpy(dest, src,(jumpOperatorP ? jumpOperatorP:nullCharP)-src);
+                }
+            }
+            if(jumpOperatorP) {
+                //TODO: we can use parameteriezed macro
+                char *dest=CIns->tokenType.c->jump;
+                char *src=++jumpOperatorP;
+                memcpy(dest, src, nullCharP-src);
+            }
+            append(&ins, CIns);
+            //add to map
+        }
+        cur=cur->next;
+    }
+
+    return 0;
+}

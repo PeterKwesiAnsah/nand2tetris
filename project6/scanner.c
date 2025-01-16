2f3512a819f2eb436a14222b91a2b3854f27d0cd
peterkwesiansah <ansahpeter123@yahoo.com>
2025-01-16 09:21:40 +0000

    fix: add only byte string with alphabets

--- project6/scanner.c
+++ project6/scanner.c
@@ -2,14 +2,17 @@
 #include "scanner.h"
 #include "symbolTable.h"
 #include "parser.h"
+#include <string.h>
 #include <sys/_types/_null.h>
+#include <stdlib.h>
 #define VARIABLE_ADDRESS 16
 
 
 unsigned address=VARIABLE_ADDRESS;
 void labelScan(struct Tokens *ins){
     int i;
-    for(i=0;i<ins->count;i++){
+    int count=ins->count;
+    for(i=0;i<count;i++){
         if(ins->darray[i]->tokenKind==LABEL){
             //set goto address to sym table
             //each instruction has line i+1
@@ -23,7 +26,12 @@ void variableScan(struct Tokens *ins){
     int i;
     for(i=0;i<ins->count;i++){
         char *variable=ins->darray[i]->tokenType.address;
-        if(ins->darray[i]->tokenKind==A && getSymValue(variable)==NULL){
+        char *p_end=NULL;
+        if(*variable=='0'&& strlen(variable)==1){
+            continue;
+        }
+        unsigned long numStr=strtoul(variable, &p_end,10);
+        if(ins->darray[i]->tokenKind==A && getSymValue(variable)==NULL && numStr==0){
             setSymValue(variable, address++);
         }
     }
